d172af4e2bd2ece06832c54131ae918e49de9b74
Eugene Yang <eugene2528@gmail.com>
2017-01-04 17:44:19 -0500

    use lock for single thread

--- paper_downloader_frame.js
+++ paper_downloader_frame.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var fs = require("fs");
 
+var lock = false;
+var useLock = false;
 
 // need to include targetDir(string), listURL(string), parser(function[saver, rawData]) 
 module.exports = {
@@ -12,26 +14,50 @@ module.exports = {
 
 		var realSaver = _saver.bind( null, config.targetDir, baseURL )
 		var realParse = config.parser.bind(null, realSaver);
-		_initGet( config.listURL, realParse );
+		if( config.pause )
+			useLock = true;
+		_initGet( config.listURL, realParse, config.pause || -1 );
 	},
-	getPage: function(link, parse){ _initGet(link, parse) }
+	getPage: (link, parse) => _initGet(link, parse),
+	Tools: {
+		getDomain: (url) => _getDomain(url)
+	}
 }
 
-function _initGet(link, parse){
-	http.get(link, (res) => {
-		if( res.statusCode !== 200 ){
-			console.log("Request Fail.");
-			res.resume();
-			return ;
-		}
-		res.setEncoding("utf8");
-		let raw = "";
-		res.on('data', (chunk) => raw += chunk);
-		res.on('end', () => parse(raw) )
+function _initGet(link, parse, pause){
 
-	}).on('error', (e) => {
-		console.log(`Got error: ${e.message}`);
-	});
+	if( useLock ){
+		var setVar = setInterval(function(){
+			if( lock == false ){
+				lock = true;
+				clearInterval(setVar);
+				run(link, function(){ lock = false })
+			}
+		}, pause);
+	}
+	else {
+		run(link);
+	}
+
+	function run(link, callback){
+		http.get(link, (res) => {
+			callback && callback()
+
+			if( res.statusCode !== 200 ){
+				console.log("Request Fail.", res.statusCode, link);
+				res.resume();
+				return ;
+			}
+			res.setEncoding("utf8");
+			let raw = "";
+			res.on('data', (chunk) => raw += chunk);
+			res.on('end', () => parse(raw) )
+
+		}).on('error', (e) => {
+			callback && callback()
+			console.log(`Got error: ${e.message}`, link);
+		});
+	}
 }
 
 function _saver(targetDir, baseurl, url, authors, title){
